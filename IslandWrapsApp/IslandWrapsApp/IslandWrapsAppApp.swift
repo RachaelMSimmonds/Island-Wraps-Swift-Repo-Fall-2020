8be61393dd153176811f69a940b14b9989c0adce
RachaelMSimmonds <36174233+RachaelMSimmonds@users.noreply.github.com>
2020-10-08 17:44:10 -0400

    Real upload of project

---
+++ IslandWrapsApp/IslandWrapsApp/IslandWrapsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IslandWrapsAppApp.swift
+//  IslandWrapsApp
+//
+//  Created by Rachael Marie Simmonds on 10/8/20.
+//
+
+import SwiftUI
+
+@main
+struct IslandWrapsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
