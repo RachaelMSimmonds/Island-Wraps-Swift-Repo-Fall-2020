488385bddf8ae110874a14c51c8dcf872f69a2a1
Rachael Simmonds <rms1252@rit.edu>
2020-10-08 18:11:07 -0400

    I am testing this to see if it work! - Rachael

--- IslandWrapsApp/IslandWrapsApp/ContentView.swift
+++ IslandWrapsApp/IslandWrapsApp/ContentView.swift
@@ -8,9 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counterNumber = 0
     var body: some View {
-        Text("Hello, world!")
+        Text("This is a test!")
             .padding()
+        HStack{
+            Button(action: {
+                counterNumber += 1
+
+//                    if counterNumber < 10 {
+//                        counterNumber += 1
+//                    } else if counterNumber >= 10 {
+//                        ZStack {
+//                            Text("Reduce your count!")
+//                                .background(Color.red)
+//                                //.padding(.bottom, 40)
+//                             }
+//                    }
+                
+            }) {
+                Image(systemName: "plus")
+                    .font(
+                        .system(size: 20, weight:
+                            .regular)
+                    )
+                    .padding(.leading, 50)
+                    .padding(.trailing, 50)
+                    .padding(.bottom, 50)
+                    .padding(.top, 50)
+                    .imageScale(.large)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(10)
+            } // end of green button
+            .offset(x: -10, y: -160)
+            // End of green button HStack
+    
+            Button(action: {
+                // action here
+                counterNumber -= 1
+            }) {
+                Image(systemName: "minus")
+                    .font(
+                        .system(size: 20, weight:
+                            .regular)
+                    )
+                    .padding(.leading, 50)
+                    .padding(.trailing, 50)
+                    .padding(.bottom, 60)
+                    .padding(.top, 60)
+                    .imageScale(.large)
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .cornerRadius(10)
+            } // end of red button
+            .offset(x: 10, y: -160)
+        
+        }// End of HStack with buttons
     }
 }
 
