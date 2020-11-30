7ceaa1dd6f7f9e7c83862618841e36b63d8e5363
Alex Ketavongsa <ak5297@rit.edu>
2020-11-29 22:09:26 -0500

    Added buttons to social media links.

--- IslandWrapsApp/IslandWrapsApp/Views/SocialView.swift
+++ IslandWrapsApp/IslandWrapsApp/Views/SocialView.swift
@@ -6,8 +6,14 @@
 //
 
 import SwiftUI
+import SafariServices
 
 struct SocialView: View {
+    // whether or not to show the Safari ViewController
+    @State var showSafari = false
+    // initial URL string
+    @State var link = "https://www.facebook.com/islandwraps2go/reviews/"
+    
     var body: some View {
         ZStack {
             Color.black
@@ -28,25 +34,70 @@ struct SocialView: View {
                 .foregroundColor(.white)
                 .offset(x: 0, y: -150)
             
-            VStack {
-                
+            
+            VStack(spacing: 30) {
+                Spacer()
+                    .frame(height: 200)
                 // Make These buttons to the social links I sent you
+                Button(action: {
+                    // update the URL if you'd like to
+                    self.link = "https://www.facebook.com/islandwraps2go/reviews/"
+                    // tell the app that we want to show the Safari VC
+                    self.showSafari = true
+                }) {
                 Image("fb_logo")
                     .resizable()
                     .frame(width: 100, height: 100)
-                    .offset(x: 0, y: 90)
+                }
+                .sheet(isPresented: $showSafari) {
+                            SafariView(url:URL(string: self.link)!)
+                        }
+                Button(action: {
+                    // update the URL if you'd like to
+                    self.link = "https://www.instagram.com/island_wraps/"
+                    // tell the app that we want to show the Safari VC
+                    self.showSafari = true
+                }) {
                 Image("ig_logo")
                     .resizable()
                     .frame(width: 100, height: 100)
-                    .offset(x: 0, y: 100)
+                }
+                .sheet(isPresented: $showSafari) {
+                            SafariView(url:URL(string: self.link)!)
+                        }
+                .buttonStyle(BorderlessButtonStyle())
+                Button(action: {
+                    // update the URL if you'd like to
+                    self.link = "https://twitter.com/islandwraps2go"
+                    // tell the app that we want to show the Safari VC
+                    self.showSafari = true
+                }) {
                 Image("Twitter")
                     .resizable()
                     .frame(width: 100, height: 100)
-                    .offset(x: 0, y: 110)
+                }
+                .sheet(isPresented: $showSafari) {
+                            SafariView(url:URL(string: self.link)!)
+                        }
                 
-            } // End of VStack for Social Medias
+            }
+            // End of VStack for Social Medias
         }
     }
+}
+
+struct SafariView: UIViewControllerRepresentable {
+
+    let url: URL
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+
+    }
+
 }
 
 struct SocialView_Previews: PreviewProvider {
