81f2db0ce2d82e66a9d276fbc9b0792618c20449
Rachael Simmonds <rms1252@rit.edu>
2020-11-30 19:51:37 -0500

    Fixed Homepage and Map functionality

--- IslandWrapsApp/IslandWrapsApp/Views/MenuListView.swift
+++ IslandWrapsApp/IslandWrapsApp/Views/MenuListView.swift
@@ -24,29 +24,35 @@ struct MenuListView: View {
         UINavigationBar.appearance().compactAppearance = navBarAppearance
         UINavigationBar.appearance().backgroundColor = .yellow
         UINavigationBar.appearance().tintColor = .black
-    }
+    } // End of init
     
     
     var body: some View {
-        NavigationView {
-            VStack {
-                if menus.list.isEmpty {
-                    // could be any view, etc
-                    Text("Oops, looks like there's no data...")
-                } else {
-                    // List will go here
-                    List(menus.list) { m in
-                        
-                        NavigationLink(destination: MenuDetailView(menu: m, selectedTab: $selectedTab)) {
-                            MenuRow(m:m)
-                        }
-                    } // List
-                } // else
-            } // VStack
-            .navigationBarTitle(Text("Menu Items"), displayMode: .automatic)
-        } // NavigationView
-    }
-}
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            NavigationView {
+               
+                    VStack {
+                        if menus.list.isEmpty {
+                            // could be any view, etc
+                            Text("Oops, looks like there's no data...")
+                        } else {
+                            // List will go here
+                            List(menus.list) { m in
+                                
+                                NavigationLink(destination: MenuDetailView(menu: m, selectedTab: $selectedTab)) {
+                                    MenuRow(m:m)
+                                }
+                            } // List
+                        } // else
+                    } // VStack
+                    .navigationBarTitle(Text("Menu Items"), displayMode: .automatic)
+                
+            } // NavigationView
+        } // End of ZStack
+    } // End of Body
+} // End of MenuListView
 
 struct MenuListView_Previews: PreviewProvider {
     static var previews: some View {
--- IslandWrapsApp/IslandWrapsApp/Views/MyMapTab.swift
+++ IslandWrapsApp/IslandWrapsApp/Views/MyMapTab.swift
@@ -1,58 +1,68 @@
+////
+////  MyMapTab.swift
+////  IslandWrapsApp
+////
+////  Created by Alex Ketavongsa on 11/12/20.
+////
+
+// THIS BREAKS THE CODE DO NOT USE - Rachael Simmonds
 //
-//  MyMapTab.swift
-//  IslandWrapsApp
+//import SwiftUI
+//import CoreLocation
 //
-//  Created by Alex Ketavongsa on 11/12/20.
+//struct MyMapTab: View {
+//    
+//    @EnvironmentObject var events: Events
+//    @State private var locationManager = CLLocationManager()
+//    @State private var showMapAlert = false
+//    @State private var selectedSegment = 0
+//    @State private var mapSelect = [MapTypeSelect(title: "Standard", map: .standard), MapTypeSelect(title: "Satellite", map: .satellite), MapTypeSelect(title: "Hybrid", map: .hybrid)]
+//    @State private var zoomed = false
+//    @ObservedObject var lm = LocationManager()
+//    
+//    var loactionError: Bool {
+//        lm.locationError ?? false
+//    }
+//    
+//    var selectedEvent: Event? {
+//        events.selectedEvent
+//    }
+//    
+//    @State private var mapView = MapView(events: $events, locationManager: $locationManager, showMapAlert: $showMapAlert, mapType: $mapSelect, zoomed: $zoomed)
+//    
+//    var alert: Alert {
+//            Alert(title: Text("Oops"), message: Text("Error Message"), dismissButton: .default(Text("Dismiss")))
+//        }
+//    
+//    var body: some View {
+//        mapView
+//            .alert(isPresented: .constant(loactionError)){
+//                Alert(title: Text("Location Access Denied"), message: Text("Your location is needed"), primaryButton: .cancel(), secondaryButton: .default(Text("Settings"), action: { goToDeviceSettings()}))
+//            }
+//            .onAppear {
+//                if (selectedEvent != nil) {
+//                    mapView
+//                        .zoomIn(annotation: selectedEvent!)
+//                    events.selectedEvent = nil
+//                } else {
+//                    mapView.zoomOut()
+//                }
+//            } // End of onAppear
+//    }
+//}
 //
-
-import SwiftUI
-import CoreLocation
-
-struct MyMapTab: View {
-    
-    @EnvironmentObject var events: Events
-    
-    @ObservedObject var lm = LocationManager()
-    
-    var loactionError: Bool {
-        lm.locationError ?? false
-    }
-    
-    var selectedEvent: Event? {
-        events.selectedEvent
-    }
-    
-    @State private var mapView = MapView()
-    
-    var body: some View {
-        mapView
-            .alert(isPresented: .constant(loactionError)){
-                Alert(title: Text("Location Access Denied"), message: Text("Your location is needed"), primaryButton: .cancel(), secondaryButton: .default(Text("Settings"), action: { goToDeviceSettings()}))
-            }
-            .onAppear {
-                if (selectedEvent != nil) {
-                    mapView
-                        .zoomIn(annotation: selectedEvent!)
-                    events.selectedEvent = nil
-                } else {
-                    mapView.zoomOut()
-                }
-            } // End of onAppear
-    }
-}
-
-extension MyMapTab {
-    // Path to device settings if location is disabled
-    func goToDeviceSettings() {
-        guard let url = URL.init(string: UIApplication.openSettingsURLString) else {
-            return
-        }
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-    }
-}
-
-struct MyMapTab_Previews: PreviewProvider {
-    static var previews: some View {
-        MyMapTab()
-    }
-}
+//extension MyMapTab {
+//    // Path to device settings if location is disabled
+//    func goToDeviceSettings() {
+//        guard let url = URL.init(string: UIApplication.openSettingsURLString) else {
+//            return
+//        }
+//        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+//    }
+//}
+//
+//struct MyMapTab_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MyMapTab()
+//    }
+//}
